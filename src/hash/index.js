5c76d474cfc18409bba468742fa2d49dbde53423
QuangChanVi <vi.quangchan@gameloft.com>
2023-05-05 11:19:14 +0700

    Update

--- src/hash/index.js
+++ src/hash/index.js
@@ -1,7 +1,33 @@
 const { createHash } = require('crypto')
 
-console.log(createHash('sha1').update('musnico@libero.it', 'binary').digest('hex'));
-setTimeout(()=>{
+const  bcrypt = require('bcryptjs')
 
-    console.log(createHash('sha1').update('musnico@libero.it', 'binary').digest('hex'));
-},1000)
+const bcrypt1 = require ('bcrypt')
+// console.log(createHash('sha1').update('musnico@libero.it', 'binary').digest('hex'));
+// setTimeout(()=>{
+// 
+//     console.log(createHash('sha1').update('musnico@libero.it', 'binary').digest('hex'));
+// },1000)
+
+const hashedPassword = bcrypt.hashSync('superadmin', '$2a$10$CwTycUXWue0Thq9StjUM0u') // hash created previously created upon sign up
+console.log(hashedPassword)
+
+
+async function isMatch(password, hashedPassword) {
+    return await bcrypt1.compare(password, hashedPassword)
+}
+async function init(password) {
+    const salt = await bcrypt1.genSalt()
+    const hashedPassword = await bcrypt1.hash(password, salt)
+    return hashedPassword
+}
+
+init(hashedPassword)
+    .then(text => {
+        console.log(text);
+    })
+
+isMatch(hashedPassword, '$2b$10$GU/Y7iD1Wa1z.YAvEXlqrOLNPIDjUFxPnmHD4zzLs3KU5yycxnQWC')
+    .then(text => {
+        console.log(text);
+    })
