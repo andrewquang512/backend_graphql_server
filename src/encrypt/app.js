bcbf4f39bc457e5c2ef2500cab0b18897fd67ca0
andrewquang512 <andrew.viquang@gmail.com>
2023-06-15 23:31:11 +0700

    Test

--- src/encrypt/app.js
+++
@@ -1,5 +0,0 @@
-const AES256Cryptor = require("./index");
-
-const testCryptor = new AES256Cryptor({ key: '2e42b098d064b3551cca98cfc9a13304', iv: 'cccfb96057bddb07' })
-
-console.log(testCryptor.decrypt('e0d12d3bd170824f51d7515d639df256185c5358b3da2124c3fdff8e026c14c8e3c959bde8336f18bda3c0967f585e25ceaa7d1835dc3bc2d7376946bbbe984b9cd6da215413beddef51b0623fd80fa6'))
--- src/nestjs-airship-parser/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './airship-service.port'
-export * from './airship-service.providers'
-export * from './sisal.airship-service'
