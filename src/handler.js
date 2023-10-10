3841119cc72852cfd4f5aaca2c1d3bcb07a8049d
QuangChanVi <vi.quangchan@gameloft.com>
2023-10-10 10:22:49 +0700

    Update

--- src/handler.js
+++ src/handler.js
@@ -1,14 +1,11 @@
 // graphql.js
 
 import { ApolloServer } from 'apollo-server';
-import {
-  startServerAndCreateLambdaHandler,
-  handlers,
-} from '@as-integrations/aws-lambda';
 import { PrismaClient } from '@prisma/client';
 import typeDefs from './Type_Definitions/_typeDefs.js';
 import resolvers from './resolvers/resolvers.js';
 import { loggingPlugin } from './logging.js';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 // Construct a schema, using GraphQL schema language
 const prisma = new PrismaClient({
@@ -20,12 +17,13 @@ const prisma = new PrismaClient({
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  plugins: [...(parseInt(process.env.IS_LOGGING) ? [loggingPlugin] : [])],
+  csrfPrevention: true,
+  cache: 'bounded',
+  plugins: [
+    ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    ...(parseInt(process.env.IS_LOGGING) ? [loggingPlugin] : []),
+  ],
   logger: console,
 });
 
-export const handler = startServerAndCreateLambdaHandler(
-  server,
-  // We will be using the Proxy V2 handler
-  handlers.createAPIGatewayProxyEventV2RequestHandler(),
-);
+export const handler = server.createHandler();
