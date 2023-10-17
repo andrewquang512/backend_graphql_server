005cf03c8b6577e9445ad140b413509567072727
andrewquang512 <andrew.viquang@gmail.com>
2023-10-17 23:20:51 +0700

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,6 @@
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-// import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
-//
-// const __dirname = path.dirname(fileURLToPath(import.meta.url));
-//
-// export default {
-//   entry: './src/handler.js',
-//   output: {
-//     path: path.resolve(__dirname, './dist'),
-//     filename: 'handler.js',
-//   },
-//   experiments: {
-//     outputModule: true,
-//   },
-//   plugins: [new NodePolyfillPlugin()],
-//   module: {
-//     // https://webpack.js.org/loaders/babel-loader/#root
-//     rules: [
-//       {
-//         test: /.m?js$/,
-//         loader: 'babel-loader',
-//         exclude: /node_modules/,
-//       },
-//     ],
-//   },
-//   resolve: {
-//     fallback: {
-//       fs: false,
-//       async_hooks: false,
-//       net: false,
-//       path: false,
-//       zlib: false,
-//       http: false,
-//       https: false,
-//       stream: false,
-//       crypto: false,
-//     },
-//   },
-//   devtool: 'source-map',
-// };
+import nodeExternals from 'webpack-node-externals';
+import CopyPlugin from 'copy-webpack-plugin';
 
-// webpack.config.js
 export default {
   target: 'node',
   entry: './src/handler.js',
@@ -63,4 +23,12 @@ export default {
       },
     ],
   },
+  externals: [nodeExternals()],
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: './node_modules/.prisma/client/schema.prisma', to: './' }, // you may need to change `to` here.
+      ],
+    }),
+  ],
 };
