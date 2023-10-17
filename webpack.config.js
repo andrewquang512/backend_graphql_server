40cccc3cb8a5e32b666e10936de582b7d750ebd0
andrewquang512 <andrew.viquang@gmail.com>
2023-10-17 22:56:30 +0700

    Add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,66 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+// import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
+//
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
+//
+// export default {
+//   entry: './src/handler.js',
+//   output: {
+//     path: path.resolve(__dirname, './dist'),
+//     filename: 'handler.js',
+//   },
+//   experiments: {
+//     outputModule: true,
+//   },
+//   plugins: [new NodePolyfillPlugin()],
+//   module: {
+//     // https://webpack.js.org/loaders/babel-loader/#root
+//     rules: [
+//       {
+//         test: /.m?js$/,
+//         loader: 'babel-loader',
+//         exclude: /node_modules/,
+//       },
+//     ],
+//   },
+//   resolve: {
+//     fallback: {
+//       fs: false,
+//       async_hooks: false,
+//       net: false,
+//       path: false,
+//       zlib: false,
+//       http: false,
+//       https: false,
+//       stream: false,
+//       crypto: false,
+//     },
+//   },
+//   devtool: 'source-map',
+// };
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
+// webpack.config.js
 export default {
+  target: 'node',
   entry: './src/handler.js',
+  mode: 'production',
   output: {
-    path: path.resolve(__dirname, './dist'),
     filename: 'handler.js',
+    libraryTarget: 'commonjs2',
   },
-  experiments: {
-    outputModule: true,
-  },
-  plugins: [new NodePolyfillPlugin()],
   module: {
-    // https://webpack.js.org/loaders/babel-loader/#root
     rules: [
       {
-        test: /.m?js$/,
-        loader: 'babel-loader',
+        test: /\.m?js$/,
         exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
     ],
   },
-  resolve: {
-    fallback: {
-      fs: false,
-      async_hooks: false,
-      net: false,
-      path: false,
-      zlib: false,
-      http: false,
-      https: false,
-      stream: false,
-      crypto: false,
-    },
-  },
-  devtool: 'source-map',
 };
